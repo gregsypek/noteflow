7eca979f2bfc3c27124bfc589bb2adcb6ecf8b1a
Greg <47268138+gregsypek@users.noreply.github.com>
2025-04-25 10:54:45 +0200

    Create question action

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -5,8 +5,6 @@ import HomeFilter from "@/components/filters/HomeFilter";
 import LocalSearch from "@/components/search/LocalSearch";
 import { Button } from "@/components/ui/button";
 import ROUTES from "@/constants/routes";
-import handleError from "@/lib/handlers/error";
-import { api } from "@/lib/api";
 import { auth } from "@/auth";
 
 const notes = [
