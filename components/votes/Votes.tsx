257cbcd547e62e59dc65729f0a4cab28a5181412
Greg <47268138+gregsypek@users.noreply.github.com>
2025-09-11 10:52:52 +0200

    Implement Votes UI

---
+++ components/votes/Votes.tsx
@@ -0,0 +1,90 @@
+"use client";
+
+import Image from "next/image";
+import { useSession } from "next-auth/react";
+import React, { useState } from "react";
+
+import { toast } from "@/hooks/use-toast";
+import { formatNumber } from "@/lib/utils";
+
+interface Params {
+  upvotes: number;
+  downvotes: number;
+  hasupVoted: boolean;
+  hasdownVoted: boolean;
+}
+
+const Votes = ({ upvotes, downvotes, hasupVoted, hasdownVoted }: Params) => {
+  const session = useSession();
+  const userId = session.data?.user?.id;
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleVote = async (voteType: "upvote" | "downvote") => {
+    if (!userId)
+      return toast({
+        title: "You need to be logged in to vote",
+        description: "Please login to vote",
+      });
+
+    setIsLoading(true);
+
+    try {
+      const successMessage =
+        voteType === "upvote"
+          ? `Upvote ${!hasupVoted ? "added" : "removed"} successfully`
+          : `Downvote ${!hasdownVoted ? "added" : "removed"} successfully`;
+
+      toast({
+        title: successMessage,
+        description: "Thank you for your feedback!",
+      });
+    } catch {
+      toast({
+        title: "Failed to vote",
+        description: "Something went wrong. Please try again.",
+        variant: "destructive",
+      });
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  return (
+    <div className="flex-center gap-2.5">
+      <div className="flex-center gap-1.5">
+        <Image
+          src={hasupVoted ? "/icons/upvoted.svg" : "/icons/upvote.svg"}
+          width={18}
+          height={18}
+          alt="upvote"
+          className={`cursor-pointer ${isLoading && "opacity-50"}`}
+          aria-label="upvote"
+          onClick={() => !isLoading && handleVote("upvote")}
+        />
+        <div className="flex-center background-light700_dark400 min-w-5 rounded-sm p-1">
+          <p className="subtle-medium text-dark400_light900">
+            {formatNumber(upvotes)}
+          </p>
+        </div>
+      </div>
+      <div className="flex-center gap-1.5">
+        <Image
+          src={hasdownVoted ? "/icons/downvoted.svg" : "/icons/downvote.svg"}
+          width={18}
+          height={18}
+          alt="downvote"
+          className={`cursor-pointer ${isLoading && "opacity-50"}`}
+          aria-label="downvote"
+          onClick={() => !isLoading && handleVote("downvote")}
+        />
+        <div className="flex-center background-light700_dark400 min-w-5 rounded-sm p-1">
+          <p className="subtle-medium text-dark400_light900">
+            {formatNumber(downvotes)}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Votes;
