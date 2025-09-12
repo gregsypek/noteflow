d2bd20c6907fc874a0afd06dc08dc5edd082af94
Greg <47268138+gregsypek@users.noreply.github.com>
2025-09-12 11:02:37 +0200

    Implement hasVoted action

--- lib/actions/vote.action.ts
+++ lib/actions/vote.action.ts
@@ -1,12 +1,22 @@
-("use server");
+"use server";
 
 import mongoose, { ClientSession } from "mongoose";
 
 import { Answer, Question, Vote } from "@/database";
+import {
+  CreateVoteParams,
+  HasVotedParams,
+  HasVotedResponse,
+  UpdateVoteCountParams,
+} from "@/types/action";
 
 import action from "../handlers/action";
 import handleError from "../handlers/error";
-import { CreateVoteSchema, UpdateVoteCountSchema } from "../validations";
+import {
+  CreateVoteSchema,
+  HasVotedSchema,
+  UpdateVoteCountSchema,
+} from "../validations";
 
 export async function updateVoteCount(
   params: UpdateVoteCountParams,
@@ -104,6 +114,48 @@ export async function createVote(
   } catch (error) {
     await session.abortTransaction();
     session.endSession();
+    return handleError(error) as ErrorResponse;
+  }
+}
+
+export async function hasVoted(
+  params: HasVotedParams
+): Promise<ActionResponse<HasVotedResponse>> {
+  const validationResult = await action({
+    params,
+    schema: HasVotedSchema,
+    authorize: true,
+  });
+
+  if (validationResult instanceof Error) {
+    return handleError(validationResult) as ErrorResponse;
+  }
+
+  const { targetId, targetType } = validationResult.params!;
+  const userId = validationResult.session?.user?.id;
+  if (!userId) handleError(new Error("Unauthorized")) as ErrorResponse;
+
+  try {
+    const vote = await Vote.findOne({
+      author: userId,
+      actionId: targetId,
+      actionType: targetType,
+    });
+
+    if (!vote) {
+      return {
+        success: false,
+        data: { hasupVoted: false, hasdownVoted: false },
+      };
+    }
+    return {
+      success: true,
+      data: {
+        hasupVoted: vote.voteType === "upvote",
+        hasdownVoted: vote.voteType === "downvote",
+      },
+    };
+  } catch (error) {
     return handleError(error) as ErrorResponse;
   }
 }
